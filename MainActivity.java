328870bfd54638bf2ccd461d5f7337741206d34e
BaranovKM <irongrol@gmail.com>
2016-08-26 15:57:33 +0400

    некий текст

---
+++ MainActivity.java
@@ -0,0 +1,13 @@
+package com.konstantin.gitallfromstudio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//некий текст для проверки коммита
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
